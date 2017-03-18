b0d02848eed54a074adac769712802bddfa0dfc6
dmlesc <dmlurth@gmail.com>
2017-03-18 13:36:09 -0600

    formatted logger.js

--- php/logger.js
+++ php/logger.js
@@ -49,118 +49,144 @@ function checkUser() {
   }
   return false;
 }
-function admitted(){
+function admitted() {
   hideID("who");
   getID("oneFace").style.display = "block";
-  giveFocus("oneInput_node");}
-function listenForEnter(event){
-    if (event.keyCode == 13)
-      logIT();}
-function notifynobody(){logIT();}
-function notifyAllIT(){logIT("AllIT");}
-function notifySMSIT(){logIT("SMSIT");}
-function logIT(notifyWho){
-    node = getID("oneInput_node").value;
-    what = getID("oneInput_what").value;
-    text = who + " - " + node + " - " + what;
-    notify = notifyWho;
-    createAction();}
-function createAction(){
-    hideID(currentFace);
-    showID("display");
-    currentID = "display";
-    createActionRecur();}
-function createActionRecur(){
-    if (index < text.length){
-      str += text.charAt(index);
-      getID(currentID).innerHTML = str;
-      index++;
-      createActionOn = setTimeout("createActionRecur()",50);}
-    else {
-      clearTimeout(createActionOn);
-      index = 0;
-      str = "";
-      logAction();}}
-function logAction(){
-    ajax = new XMLHttpRequest();
-    var url = "logIT.php?who=" + who + "&node=" + node + "&what=" + what;
-    if (notify)
-      url += "&notify=" + notify;
-    ajax.onreadystatechange=confirmITlogged;
-    ajax.open("GET", url, true);
-    ajax.send();}
-function confirmITlogged(){
-    if (ajax.responseText == "success")
-      timer();}
-function timer(){timerRecur();}
-function timerRecur(){
-    if (seconds < 2){
-      seconds++;
-      timerOn = setTimeout("timerRecur()",1000);}
-    else
-      timerStop();}
-function timerStop(){
-clearTimeout(timerOn);
-seconds = 0;
-    toTheCloud();}
-function toTheCloud(){
-    moveRight();
-    moveUp();}
-function moveRight(){moveRightRecur();}
-function moveRightRecur(){
-    if (moves < moveSteps){
-      moves += moveRate;
-      getID("display").style.left = moves;
-      moveRightOn = setTimeout("moveRightRecur()",5);}
-    else {
-      clearTimeout(moveRightOn);
-      moves = 5;
-      hideID("display");
-      getID("display").style.left = moves;}}
-function moveUp(){
-    moveUpRecur();}
-function moveUpRecur(){
-    if (moveUpCur > -20){
-      moveUpCur--;
-      getID("display").style.top = moveUpCur;
-      moveUpOn = setTimeout("moveUpRecur()",5);}
-    else {
-      clearTimeout(moveUpOn);
-      moveUpCur = 30;
-      hideID("display");
-      getID("display").style.top = moveUpCur;
-      backToFace();}}
-function backToFace(){
-    showID(currentFace);
-    getID("oneInput_node").value = "";
-    getID("oneInput_what").value = "";
-    giveFocus("oneInput_node");}
-function resizeSmall(id){
+  giveFocus("oneInput_node");
+}
+function listenForEnter(event) {
+  if (event.keyCode == 13)
+    logIT();
+}
+function notifynobody() { logIT(); }
+function notifyAllIT() { logIT("AllIT"); }
+function notifySMSIT() { logIT("SMSIT"); }
+function logIT(notifyWho) {
+  node = getID("oneInput_node").value;
+  what = getID("oneInput_what").value;
+  text = who + " - " + node + " - " + what;
+  notify = notifyWho;
+  createAction();
+}
+function createAction() {
+  hideID(currentFace);
+  showID("display");
+  currentID = "display";
+  createActionRecur();
+}
+function createActionRecur() {
+  if (index < text.length) {
+    str += text.charAt(index);
+    getID(currentID).innerHTML = str;
+    index++;
+    createActionOn = setTimeout("createActionRecur()", 50);
+  }
+  else {
+    clearTimeout(createActionOn);
+    index = 0;
+    str = "";
+    logAction();
+  }
+}
+function logAction() {
+  ajax = new XMLHttpRequest();
+  var url = "logIT.php?who=" + who + "&node=" + node + "&what=" + what;
+  if (notify)
+    url += "&notify=" + notify;
+  ajax.onreadystatechange=confirmITlogged;
+  ajax.open("GET", url, true);
+  ajax.send();
+}
+function confirmITlogged() {
+  if (ajax.responseText == "success")
+    timer();
+}
+function timer() { timerRecur(); }
+function timerRecur() {
+  if (seconds < 2) {
+    seconds++;
+    timerOn = setTimeout("timerRecur()",1000);
+  }
+  else
+    timerStop();
+}
+function timerStop() {
+  clearTimeout(timerOn);
+  seconds = 0;
+  toTheCloud();
+}
+function toTheCloud() {
+  moveRight();
+  moveUp();
+}
+function moveRight() { moveRightRecur(); }
+function moveRightRecur() {
+  if (moves < moveSteps) {
+    moves += moveRate;
+    getID("display").style.left = moves;
+    moveRightOn = setTimeout("moveRightRecur()", 5);
+  }
+  else {
+    clearTimeout(moveRightOn);
+    moves = 5;
+    hideID("display");
+    getID("display").style.left = moves;
+  }
+}
+function moveUp() {
+  moveUpRecur();
+}
+function moveUpRecur() {
+  if (moveUpCur > -20) {
+    moveUpCur--;
+    getID("display").style.top = moveUpCur;
+    moveUpOn = setTimeout("moveUpRecur()",5);
+  }
+  else {
+    clearTimeout(moveUpOn);
+    moveUpCur = 30;
+    hideID("display");
+    getID("display").style.top = moveUpCur;
+    backToFace();
+  }
+}
+function backToFace() {
+  showID(currentFace);
+  getID("oneInput_node").value = "";
+  getID("oneInput_what").value = "";
+  giveFocus("oneInput_node");
+}
+function resizeSmall(id) {
   currentFocusSmall = id;
-  resizeFocusSmallRecur();}
-function resizeFocusSmallRecur(){
-    if (focusFontSizeSmall > 14){
-      focusFontSizeSmall -= focusResizeRate;
-      getID(currentFocusSmall).style.fontSize = focusFontSizeSmall;
-      focusResizeSmallOn = setTimeout("resizeFocusSmallRecur()",3);}
-    else {
-      clearTimeout(focusResizeSmallOn);
-      focusFontSizeSmall = 18;}}
-function resizeBig(id){
+  resizeFocusSmallRecur();
+}
+function resizeFocusSmallRecur() {
+  if (focusFontSizeSmall > 14) {
+    focusFontSizeSmall -= focusResizeRate;
+    getID(currentFocusSmall).style.fontSize = focusFontSizeSmall;
+    focusResizeSmallOn = setTimeout("resizeFocusSmallRecur()", 3);
+  }
+  else {
+    clearTimeout(focusResizeSmallOn);
+    focusFontSizeSmall = 18;
+  }
+}
+function resizeBig(id) {
   currentFocusBig = id;
-  resizeFocusBigRecur();}
-function resizeFocusBigRecur(){
-    if (focusFontSizeBig < 18){
-      focusFontSizeBig += focusResizeRate;
-      getID(currentFocusBig).style.fontSize = focusFontSizeBig;
-      focusResizeBigOn = setTimeout("resizeFocusBigRecur()",3);}
-    else {
-      clearTimeout(focusResizeBigOn);
-      focusFontSizeBig = 14;}}
-function giveFocus(id){
-    getID(id).focus();}
-function getID(id){
-    return document.getElementById(id);}
-function hideID(id){
-    getID(id).style.display = "none";}
-function showID(id){ getID(id).style.display = "block";}
+  resizeFocusBigRecur();
+}
+function resizeFocusBigRecur() {
+  if (focusFontSizeBig < 18) {
+    focusFontSizeBig += focusResizeRate;
+    getID(currentFocusBig).style.fontSize = focusFontSizeBig;
+    focusResizeBigOn = setTimeout("resizeFocusBigRecur()", 3);
+  }
+  else {
+    clearTimeout(focusResizeBigOn);
+    focusFontSizeBig = 14;
+  }
+}
+function giveFocus(id) { getID(id).focus(); }
+function getID(id){ return document.getElementById(id); }
+function hideID(id) { getID(id).style.display = "none"; }
+function showID(id) { getID(id).style.display = "block"; }
